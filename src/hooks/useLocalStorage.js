c071cea83381be5c268b7232eb9ed9bd26e1fdd7
ridha mezrigui <ridhamezrigui07@gmail.com>
2022-03-18 22:46:26 +0100

    add the debounce , throttle and useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,22 +1,21 @@
-import { useMemo, useState } from "react"
+import { useMemo, useState } from 'react';
 
-export default useLocalStorage(key, data) {
-    const [storedData, setStoredData] = useState(() => {
-        if(getItem(key)) return getItem(key)
-        setItem(key,data)
-        return data
-    })
+export default useLocalStorage = (key, initialValue) => {
+  const [storedData, setStoredData] = useState(() => {
+    if (getItem(key)) return getItem(key);
+    setItem(key, initialValue);
+    return initialValue;
+  });
 
-    const getItem = key => JSON.parse(localStorage.getItem(key))
+  const getItem = (key) => JSON.parse(localStorage.getItem(key));
 
-    const setItem = (key,value) => JSON.stringify(localStorage.setItem(key,value))
+  const setItem = (key, value) =>
+    JSON.stringify(localStorage.setItem(key, value));
 
-    useMemo(() =>{
-        const data = getItem(key)
-        if(data) setStoredData(data)
-    })
+  useMemo(() => {
+    const data = getItem(key);
+    if (data) setStoredData(data);
+  });
 
-   
-
-    return {storedData, getItem, setItem}
-}
+  return { storedData, getItem, setItem };
+};
