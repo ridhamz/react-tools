45dd20b73ce780a98816cd1140737ef293f39ea1
ridha mezrigui <ridhamezrigui07@gmail.com>
2022-03-18 22:48:56 +0100

    implement the session storage hook

---
+++ src/hooks/useSessionStorage.js
@@ -0,0 +1,21 @@
+import { useMemo, useState } from 'react';
+
+export default useSessionStorage = (key, initialValue) => {
+  const [storedData, setStoredData] = useState(() => {
+    if (getItem(key)) return getItem(key);
+    setItem(key, initialValue);
+    return initialValue;
+  });
+
+  const getItem = (key) => JSON.parse(sessionStorage.getItem(key));
+
+  const setItem = (key, value) =>
+    JSON.stringify(sessionStorage.setItem(key, value));
+
+  useMemo(() => {
+    const data = getItem(key);
+    if (data) setStoredData(data);
+  });
+
+  return { storedData, getItem, setItem };
+};
